a9044597e0195b258503717d06cbb15466a77a5d
Juan Ignacio Rodríguez de León <euribates@gmail.com>
2015-11-09 00:04:15 +0000

    A?adido Scheduler

---
+++ control.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# test_actors.py
+
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+
+
+class Scheduler():
+
+    def __init__(self):
+        self.actions = {}
+        self.actors = set()
+        self.active_actions = []
+        self.frame = 0
+
+    def reset(self):
+        for actor in self.actors:
+            actor.reset()
+        self.active_actions = []
+        self.frame = 0
+
+    def add_action(self, action):
+        start_frame = action.interval.lower_bound
+        self.actors.add(action.actor)
+        key = (action.actor.name, start_frame)
+        self.actions.setdefault(key, []).append(action)
+
+    def next(self):
+        for action in self.active_actions[:]:
+            data = action(self.frame)
+            if data:
+                action.actor.state.delta(**data)
+            if action.interval.is_last(self.frame):
+                action.end(self.frame)
+                self.active_actions.remove(action)
+        for actor in self.actors:  # New actions that must start
+            key = (actor.name, self.frame)
+            if key in self.actions:
+                for a in self.actions[key]:
+                    a.start(self.frame)
+                    self.active_actions.append(a)
+
+        self.frame += 1
+        return self.frame
+
+    def dump(self, num_frames=15):
+        buff = ['\n--[Scheduler actors:{}]----------------------\n'.format(
+            len(self.actors)
+            )]
+        for f in range(num_frames):
+            buff.append('{:5d} '.format(f))
+            for actor in self.actors:
+                pk = (actor.name, f)
+                if pk in self.actions:
+                    for action in self.actions[pk]:
+                        buff.append('Starts {}'.format(
+                            action
+                            ))
+            buff.append('\n')
+        buff.append('-------------------------------------\n')
+        return ''.join(buff)
+
---
+++ test_control.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+
+import sys
+import unittest
+
+import control
+
+from actors import Square
+from actions import Fall, MoveTo
+
+class TestBob(unittest.TestCase):
+
+    def test(self):
+        bob = Square('Bob')
+        sch = control.Scheduler()
+        sch.add_action(MoveTo(bob, 0, 2, (100, 0)))
+        for i in range(15):
+            print('{:5} {}'.format(sch.frame, repr(bob)), file=sys.stderr)
+            sch.next()
+
+class TestScheduler(unittest.TestCase):
+
+    def test(self):
+
+        scheduler = control.Scheduler()
+        self.assertEqual(len(scheduler.actors), 0)
+        
+        charles = Square('Charles', pos=(100, 0), color='brown4', side=75)
+        scheduler.add_action(Fall(charles, 0, 5, (100, 300)))
+
+        self.assertEqual(len(scheduler.actors), 1)
+
+        dorothy = Square('Dorothy', pos=(0, 0), color='#32BF98', side=25)
+        scheduler.add_action(MoveTo(dorothy, 0, 5, (100, 100)))
+        self.assertEqual(len(scheduler.actors), 2)
+        
+        # Frame 0
+        self.assertEqual(charles.pos, (100, 0))
+        self.assertEqual(dorothy.pos, (0, 0))
+
+        self.assertEqual(scheduler.next(), 1) # Frame 1
+        
+        self.assertEqual(charles.pos, (100, 0))
+        self.assertEqual(dorothy.pos, (20, 20))
+
+        self.assertEqual(scheduler.next(), 2) # Frame 2
+        self.assertEqual(dorothy.state.pos, (400, 5))
+
+        self.assertEqual(scheduler.next(), 3) # Frame 3
+        self.assertEqual(dorothy.state.pos, (400, 5))
+
+        #self.assertEqual(int(round(dorothy.pos.x)), 392)
+        #self.assertEqual(int(round(dorothy.pos.y)), 12)
+
+if __name__ == '__main__':
+    unittest.main()
